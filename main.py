ca173117f3a369054b494c2e5dd919925f30ea9b
Karla <karla.gardiner@gmail.com>
2025-02-25 13:56:03 +0000

    2: added add-plant endpoint that adds plant to user

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from fastapi import FastAPI
 from config import logger
 from packages.mongodb import lifespan
@@ -9,5 +10,12 @@ app = FastAPI(lifespan=lifespan)
 async def create_plant(plant: Plant) -> Plant:
     new_plant = await app.mongodb["plants"].insert_one(plant.model_dump())
     plant.id = str(new_plant.inserted_id)
-    logger.debug('created plant')
+    return plant
+
+@app.post("/add-plant/{userId}", response_model=Plant)
+async def add_plant(userId: str, plant: Plant):
+    todays_date = '25-02-2025'
+    plant_key = 'plants.%s' % todays_date
+
+    await app.mongodb['users'].update_one( { '_id': ObjectId(userId) }, { '$push': { plant_key: plant.model_dump() } }, upsert=True )
     return plant
