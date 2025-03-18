ad42b1b3fd834717aa44199a9bd2df1afefb4b36
Karla <karla.gardiner@gmail.com>
2025-03-18 17:12:45 +0000

    4: added some blank lines

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ app = FastAPI(lifespan=lifespan)
 async def create_plant(plant: Plant) -> Plant:
     new_plant = await app.mongodb["plants"].insert_one(plant.model_dump())
     plant.id = str(new_plant.inserted_id)
+    
     return plant
 
 @app.post("/user/{userId}/add-plant/{plantId}")
@@ -20,6 +21,7 @@ async def add_plant(userId: str, plantId: str):
     plant_key = 'plants.%s' % todays_date
     
     await app.mongodb['users'].update_one( { '_id': ObjectId(userId) }, { '$addToSet': { plant_key: plant_to_add } }, upsert=True )
+    
     return 200
 
 @app.get('/user/{userId}/plants')
