0eee4a2e4b9c158db55d0c072dceaa4649558877
Karla <karla.gardiner@gmail.com>
2025-04-07 19:18:00 +0100

    116: added browser route to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { Authenticate } from './views/Authenticate';
+import { Login } from './views/Login';
 import { Today } from './views/Today';
 import './App.css';
 
 function App() {
-
   return (
-    <Today />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' Component={Login} />
+          <Route path='/oauth2/callback' Component={Authenticate} />
+          <Route path='/' Component={Today} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
