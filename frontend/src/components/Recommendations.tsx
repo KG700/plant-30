4c5f11619c6b0c5c363ed0985fc001694d3d50ea
Karla <karla.gardiner@gmail.com>
2025-04-24 15:36:40 +0100

    173: wip fetching recommendations

--- frontend/src/components/Recommendations.tsx
+++ frontend/src/components/Recommendations.tsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
 export function Recommendations() {
+  const navigate = useNavigate();
+  const [recommendations, setRecommendations] = useState({});
+  const [isFetchError, setIsFetchError] = useState(false);
+
+  const fetchRecommendations = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const data = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/user/plants/recommendations`,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": process.env.REACT_APP_ORIGIN ?? "",
+            Authorization: `Bearer ${token}`,
+          },
+          credentials: "include",
+        },
+      );
+
+      if (!data.ok) {
+        if (data.status === 401) navigate("/login");
+      }
+      const plantsData = await data.json();
+
+      setRecommendations(plantsData ?? []);
+      console.log(plantsData);
+    } catch (error) {
+      console.log({ error });
+      setIsFetchError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecommendations();
+  }, []);
+
   return <div>Recommendations!</div>;
 }
